59e2826c9c158bd742bd63eea25c4586777193ae
George <georgenechyporenko@gmail.com>
2021-05-14 19:29:22 -0700

    added templates and simple test cases

--- my_app01/urls.py
+++ my_app01/urls.py
@@ -1,11 +1,11 @@
 # my file for urls in my_app01
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
 
-    path('', homePageView, name='home'),
-
+    path('', HomePageView.as_view(), name='homepage'),
+    path('about/', AboutPageView.as_view(), name='aboutpage'),
 ]
 
 
